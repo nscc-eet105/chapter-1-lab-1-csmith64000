87326241fdd622b9be6b6ad6309937727a6fc38a
Chris Smith <christopher.smith2005@outlook.com>
2025-06-10 18:14:48 -0400

    Complete

---
+++ Lab1.py
@@ -0,0 +1,7 @@
+print("Speak the truth")
+print("Speak it all")
+print("Speak it so that all may hear")
+print("In the end it shall appear")
+print("Truth is best in age and youth.")
+print("Speak the truth.")
+      
